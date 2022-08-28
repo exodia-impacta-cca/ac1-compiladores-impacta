068504d236991a0664a2ede88fdc57136eb8d0d6
Arthur Vinicius <thuur.vss@gmail.com>
2022-08-28 12:18:36 -0300

    commit inicial

--- ex1.py
+++ ex1.py
@@ -1,7 +1,6 @@
 import os
 
-
-with open ("Lucas da Silva Santos - ex03.c", "r") as file:
+with open ("Arthur Vinicius Santos Silva - ex03.c", "r") as file:
     numeros = []
     alfabeto = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" ,"m" ,"n" ,"o" ,"p" ,"q" ,"r" ,"s" ,"t" ,"u" ,"v" ,"w" ,"x" ,"y" , "z", "1", "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "0", " "]
 
@@ -12,7 +11,7 @@ with open ("Lucas da Silva Santos - ex03.c", "r") as file:
         for i in range(len(alfabeto)):
             count[i] = count[i] + linhas.count(alfabeto[i])
 
-print("Tamanho do arquivo lido: " + str( os.stat("Lucas da Silva Santos - ex03.c").st_size) + " bytes")
+print("Tamanho do arquivo lido: " + str( os.stat("Arthur Vinicius Santos Silva - ex03.c").st_size) + " bytes")
 print("Número de linhas do arquivo lido: " + str(numLinhas))
 
 for i in range(len(alfabeto)):
--- ex2.py
+++ ex2.py
@@ -5,7 +5,7 @@
 
 import re
 
-with open ("Lucas da Silva Santos - ex03.c", "r") as file, open("ex03.out", "w+") as writefile:
+with open ("Arthur Vinicius Santos Silva - ex03.c", "r") as file, open("ex03.out", "w+") as writefile:
     alfabeto = ["main" , "auto" , "break" , "case" , "char" , "const" , "continue" , "default" , "do" , "double" , "else" , 
     "enum" ,"extern" ,"float" ,"for" ,"goto" ,"if" ,"int" ,"long" ,"register" ,"return" ,"short" ,"signed" ,"sizeof" ,"static" , "struct", "switch", "typedef" , "union" , "unsigned" , "void" , "volatile" , "while"]
     
