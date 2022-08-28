638e601c6eb0b29e2d00239d92310e636db43eaa
Arthur Vinicius <thuur.vss@gmail.com>
2022-08-28 14:06:30 -0300

    feat: ex2 ignora string
    Co-authored-by: David Ferreira <ferreiradavid361@gmail.com>

--- ex2.py
+++ ex2.py
@@ -5,7 +5,8 @@ with open ("Arthur Vinicius Santos Silva - ex03.c", "r") as entrada:
     "enum" ,"extern" ,"float" ,"for" ,"goto" ,"if" ,"int" ,"long" ,"register" ,"return" ,"short" ,"signed" ,"sizeof" ,"static" , "struct", "switch", "typedef" , "union" , "unsigned" , "void" , "volatile" , "while"]
 
     with open("ex03.out", "w+") as saida:
-        for linhas in entrada:
-            for reservada in alfabeto:
-                linhas = re.sub(r"\b" + reservada + r"\b" , reservada.upper(), linhas)
-            saida.write(linhas)
+        for linha in entrada:
+            if '"' not in linha:
+                for reservada in alfabeto:
+                    linha = re.sub(r"\b" + reservada + r"\b" , reservada.upper(), linha)
+            saida.write(linha)
